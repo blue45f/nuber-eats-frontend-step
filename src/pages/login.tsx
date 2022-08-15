ba3d09cbfe0f2fe7a81dd0d45f826f371b3e5258
hjunkim <hjunkim@woowahan.com>
2022-08-15 20:17:04 +0900

    [add] #15.11 UI Clonning part Two

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,8 @@
 import {gql, useMutation} from "@apollo/client";
 import React from "react";
 import {useForm} from "react-hook-form";
+import {Link} from "react-router-dom";
+import {Button} from "../components/button";
 import {FormError} from "../components/form-error";
 import nuberLogo from "../images/logo.svg";
 import {loginMutation, loginMutationVariables,} from "../__generated__/loginMutation";
@@ -18,11 +20,17 @@ const LOGIN_MUTATION = gql`
 interface ILoginForm {
   email: string;
   password: string;
-  resultError?: string;
 }
 
 export const Login = () => {
-  const {register, getValues, formState: {errors}, handleSubmit} = useForm<ILoginForm>();
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: {errors, isValid},
+  } = useForm<ILoginForm>({
+    mode: "onChange",
+  });
   const onCompleted = (data: loginMutation) => {
     const {
       login: {ok, token},
@@ -57,7 +65,7 @@ export const Login = () => {
         </h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid gap-3 mt-5 w-full"
+          className="grid gap-3 mt-5 w-full mb-5"
         >
           <input
             {...register("email", {required: "Email is required"})}
@@ -84,11 +92,21 @@ export const Login = () => {
           {errors.password?.type === "minLength" && (
             <FormError errorMessage="Password must be more than 10 chars."/>
           )}
-          <button className="btn">{loading ? "Loading..." : "Log In"}</button>
+          <Button
+            canClick={isValid}
+            loading={loading}
+            actionText={"Log in"}
+          />
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error}/>
           )}
         </form>
+        <div>
+          New to Nuber?{" "}
+          <Link to="/create-account" className="text-lime-600 hover:underline">
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   );
