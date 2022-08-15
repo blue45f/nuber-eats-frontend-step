3846ace09af29b552963da9a7193be42d63f4b32
hjunkim <hjunkim@woowahan.com>
2022-08-15 13:08:19 +0900

    [add] #15.2 React Hook Form part Two

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import {useForm} from "react-hook-form";
 
+interface IForm {
+  email: string;
+  password: string;
+}
+
 export const LoggedOutRouter = () => {
-  const {register, watch, handleSubmit, formState: {errors}} = useForm();
+  const {register, watch, handleSubmit, formState: {errors}} = useForm<IForm>();
   const onSubmit = () => {
     console.log(watch());
   };
   const onInvalid = () => {
     console.log("cant create account");
   };
-  console.log(errors);
   return (
     <div>
       <h1>Logged Out</h1>
       <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
         <div>
           <input
-            {...register("email", {
+            {...register('email', {
               required: "This is required",
               pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
             })}
@@ -24,10 +28,19 @@ export const LoggedOutRouter = () => {
             type="email"
             placeholder="email"
           />
+          {errors.email?.message && (
+            <span className="font-bold text-red-600">
+              {errors.email?.message}
+            </span>
+          )}
+          {errors.email?.type === "pattern" && (
+            <span className="font-bold text-red-600">Only gmail allowed</span>
+          )}
         </div>
         <div>
           <input
-            {...register("password", {required: true})}
+            {...register('password', {required: true})}
+            name="password"
             type="password"
             required
             placeholder="password"
